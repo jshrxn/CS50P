0c77ceb8d2069c6ca5fd4535eb0c2132bd6b516e
Jassher Ybañez <jshrxn@gmail.com>
2026-05-13 09:18:08 +0800

    Added CS50P Playback Speed Problem Solution

---
+++ problem_sets/week0/playback.py
@@ -0,0 +1,7 @@
+original_speed = str(input("Some people have a habit of lecturing speaking rather quickly,"
+"\nand it’d be nice to slow them down, a la YouTube’s 0.75 playback speed,"
+"\nor even by having them pause between words.\nWhat do you wish to input? "))
+
+playback_speed = original_speed.replace(" ", "...")
+
+print(playback_speed)
