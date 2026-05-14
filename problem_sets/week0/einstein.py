93d81d49afa6d5b2a9f87554c7e30a870d9e0de4
Jassher Ybañez <jshrxn@gmail.com>
2026-05-14 10:33:23 +0800

    Added my solution to the problem Einstein in Harvard's CS50P

---
+++ problem_sets/week0/einstein.py
@@ -0,0 +1,8 @@
+user_input = int(input("What do you wish to input for the mass as an integer? " ))
+
+m = user_input
+c = 300000000 * 300000000
+E = m * c
+
+print("The equivalent number of Joules as an integer based on the value of the mass inputted is", E)
+
