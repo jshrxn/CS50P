8f6ae9dc9a1c1f57f2f5d9dae3b443533a2e2e91
Jassher Ybañez <jshrxn@gmail.com>
2026-05-16 21:10:30 +0800

    Added my solution to the conditional problem named Math Interpreter in Harvard's CS50P

---
+++ problem_sets/week1/interpreter.py
@@ -0,0 +1,27 @@
+expression = input("Expression: ")
+
+x, y, z = expression.split()
+
+x = float(x)
+z = float(z)
+
+if y == "+":
+    print(x + z)
+
+elif y == "-":
+    print(x - z)
+
+elif y == "*":
+    print(x * z)
+
+elif y == "/":
+    print(x / z)
+
+else:
+    print("error")
+
+
+
+
+
+
