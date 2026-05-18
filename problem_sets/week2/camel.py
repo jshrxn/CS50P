54d03e04d5af2367c96cdc18d4840582613c79a4
Jassher Ybañez <jshrxn@gmail.com>
2026-05-18 20:49:52 +0800

    Added my solution to the problem named camelCase in Harvard's CS50P

---
+++ problem_sets/week2/camel.py
@@ -0,0 +1,18 @@
+camel = input("camelCase: ")
+
+snake = ""
+
+for letter in camel:
+
+    if letter.isupper():
+        snake += "_" + letter.lower()
+
+    else:
+        snake += letter
+
+print("snake_case:", snake)
+
+
+
+
+
