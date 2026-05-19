4dfd4c5111c6758d4b4fc34de4e76f13cbbc1a4a
Jassher Ybañez <jshrxn@gmail.com>
2026-05-19 10:48:04 +0800

    Added my solution to the problem named Coke Machine in Harvard's CS50P

---
+++ problem_sets/week2/coke.py
@@ -0,0 +1,16 @@
+amount = 50
+
+while amount > 0:
+
+    print("Amount Due:", amount)
+
+    coin = int(input("Insert Coin: "))
+
+    if coin == 25 or coin == 10 or coin == 5:
+        amount = amount - coin
+
+print("Change Owed:", abs(amount))
+
+
+
+
