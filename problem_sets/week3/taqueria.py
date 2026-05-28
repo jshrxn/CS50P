8b04dd4ef1f000a7a55b56d1552667128f46c7e3
Jassher Ybañez <jshrxn@gmail.com>
2026-05-28 21:32:20 +0800

    Added my solution to the problem named Felipe's Taqueria in Harvard's CS50P

---
+++ problem_sets/week3/taqueria.py
@@ -0,0 +1,32 @@
+menu = {
+
+    "Baja Taco": 4.25,
+    "Burrito": 7.50,
+    "Bowl": 8.50,
+    "Nachos": 11.00,
+    "Quesadilla": 8.50,
+    "Super Burrito": 8.50,
+    "Super Quesadilla": 9.50,
+    "Taco": 3.00,
+    "Tortilla Salad": 8.00
+
+}
+
+total = 0.0
+
+try:
+    while True:
+        user_input = str(input("Item: "))
+        user_input = user_input.strip().title()
+
+        if user_input in menu:
+            value = menu[user_input]
+            total += value
+            print(f"Total: ${total:.2f}")
+
+except (KeyboardInterrupt, EOFError):
+    print(f"Total: ${total:.2f}")
+
+
+
+
